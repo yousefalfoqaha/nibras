909cd7828db6da3c7e0191cc45e97128bf380e4f
Yousef Alfoqaha <yousefmustafaadam@gmail.com>
2025-12-03 13:59:35 +0300

    chore: rename endpoint

--- src/main/java/edu/gju/chatbot/gju_chatbot/Controller.java
+++ src/main/java/edu/gju/chatbot/gju_chatbot/Controller.java
@@ -3,6 +3,7 @@ package edu.gju.chatbot.gju_chatbot;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -11,12 +12,12 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/ai")
 public class Controller {
-
   private final ChatClient chatClient;
 
-  @GetMapping
-  public String streamResponse(String userInput) {
-    return chatClient.prompt(userInput)
+  @GetMapping("/generate")
+  public String streamResponse(
+      @RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+    return chatClient.prompt(message)
         .call()
         .content();
   }
