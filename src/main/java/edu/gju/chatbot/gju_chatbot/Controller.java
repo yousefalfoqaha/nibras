1f08d6504c80ae4ad22a39fa6fa0a82ea0117d74
yousefalfoqaha <yousefalfoqaha@proton.me>
2025-12-04 16:02:34 +0300

    feat: early etl pipeline, 1 pdf at a time support

--- src/main/java/edu/gju/chatbot/gju_chatbot/Controller.java
+++ src/main/java/edu/gju/chatbot/gju_chatbot/Controller.java
@@ -1,11 +1,15 @@
 package edu.gju.chatbot.gju_chatbot;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import edu.gju.chatbot.gju_chatbot.service.EtlPipelineService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -14,11 +18,21 @@ import lombok.RequiredArgsConstructor;
 public class Controller {
   private final ChatClient chatClient;
 
+  private final EtlPipelineService etlPipelineService;
+
   @GetMapping("/generate")
   public String generate(
       @RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
     return chatClient.prompt(message)
         .call()
         .content();
+  }
+
+  @PostMapping("/ingest")
+  public ResponseEntity<Void> ingestFile(
+      @RequestParam("file") MultipartFile file) {
+    etlPipelineService.processFile(file);
+
+    return ResponseEntity.ok().build();
   }
 }
