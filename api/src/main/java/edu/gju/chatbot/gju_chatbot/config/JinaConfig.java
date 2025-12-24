c356c1b70e933036d76ba7dc7c549214cb002915
yousefalfoqaha <yousefalfoqaha@proton.me>
2025-12-24 22:27:19 +0300

    fix: import

--- api/src/main/java/edu/gju/chatbot/gju_chatbot/config/JinaConfig.java
+++ api/src/main/java/edu/gju/chatbot/gju_chatbot/config/JinaConfig.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.web.client.RestClientAutoConfigura
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.client.RestClient;
 
