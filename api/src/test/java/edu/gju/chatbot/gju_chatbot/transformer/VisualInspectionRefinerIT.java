2b4316bbc315dc84f76f1f67023951c540ec0898
yousefalfoqaha <yousefalfoqaha@proton.me>
2026-01-06 16:01:34 +0300

    chore: remove printing refined markdown pages in test

--- api/src/test/java/edu/gju/chatbot/gju_chatbot/transformer/VisualInspectionRefinerIT.java
+++ api/src/test/java/edu/gju/chatbot/gju_chatbot/transformer/VisualInspectionRefinerIT.java
@@ -2,16 +2,16 @@ package edu.gju.chatbot.gju_chatbot.transformer;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.document.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
-import edu.gju.chatbot.gju_chatbot.reader.MarkdownConverter;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import edu.gju.chatbot.gju_chatbot.reader.MarkdownConverter;
 
 @SpringBootTest
 public class VisualInspectionRefinerIT {
@@ -30,12 +30,5 @@ public class VisualInspectionRefinerIT {
     List<Document> refinedPages = visualInspectionRefiner.apply(pagesWithImages);
 
     assertTrue(refinedPages.size() == (int) Math.floor(pagesWithImages.size() * 0.5));
-
-
-    String fileContent = refinedPages.stream()
-        .map(Document::getText)
-        .collect(Collectors.joining("\n"));
-
-    System.out.println(fileContent);
   }
 }
