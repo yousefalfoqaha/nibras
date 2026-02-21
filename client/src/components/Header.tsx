f6680d95c0b245d96be1d1ec36848c3c1811bde5
yousefalfoqaha <yousefalfoqaha@proton.me>
2026-02-22 00:17:36 +0300

    fix: header component not referencing build imprint

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import { Text, Image } from '@mantine/core';
 import styles from './Header.module.css';
+import nibrasLogo from '../assets/nibras.png';
 
 export function Header() {
 	return (
 		<header className={styles.header}>
 			<div className={styles.intro}>
-				<Image h={125} w={125} src="src/assets/nibras.png" />
+				<Image h={125} w={125} src={nibrasLogo} />
 				<Text>Hi, I'm <span style={{ color: 'var(--mantine-color-primary-filled)', fontWeight: 700 }}>Nibras</span></Text>
 			</div>
 
